4c8ad5b14a7e2f11e4c27ef78d6600cd231fa0ea
Pierre Caulet <pierre.caulet78@gmail.com>
2017-01-23 15:02:59 +0100

    Ajout de commentaires pour les attributs+Constructeur par défaut

--- string.h
+++ string.h
@@ -2,15 +2,15 @@
 #include <cstdlib>
 
 class String {
-  private :
-	size_t size_; //
-    size_t capacity_; // 
-    char* tab_; //
-    static const size_t max_size; //
+  protected :
+	size_t size_; //taille effective de la String
+    size_t capacity_; //capacité du tableau
+    char* tab_; //"valeur" de la String (contenu : chaine de caractères)
+    static const size_t max_size; //taille maximale du tableau/chaîne
     
   public : 
 	//Constructeurs
-    //String(); //Constructeur par défaut
+    String(); //Constructeur par défaut
 	//String(size_t n, char t); //Constructeur à partir d'une c-string
 
 	//Destructeurs
