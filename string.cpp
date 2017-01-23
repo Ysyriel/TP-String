2a6ecfef7c4b0e27cf631336557b6619a1cbcd7c
Adrien Stadler <adrien-stadler@hotmail.fr>
2017-01-23 15:54:46 +0100

    Constructeur par c-string, Nécessité des méthodes length et getCapacity

--- string.cpp
+++ string.cpp
@@ -4,11 +4,41 @@
 
 const size_t String::max_size = 100; //Définition de l'attribut static
 
-String::String(){
+//Constructeurs
+String::String(){ //Constructeur par défaut
 	size_=0;
 	capacity_=0;
 	tab_=new char[capacity_+1];
 	tab_[0]='\0';
 }
 
+String::String(char* c_string){ // Constructeur par c-string
+	size_=length(c_string);
+	capacity_=getCapacity(size_);
+	tab_=new char[capacity_+1];
+	for (unsigned int i=0; i<size_; i++){
+			tab_[i]=c_string[i];
+	}	
+	tab_[size_]='\0';
+}	
+//Destructeurs
 
+//Méthodes protégées
+size_t String::length(const char* c){
+	size_t len=0;
+	while (c[len] != '\0' || len <= max_size){
+		if (len=max_size){
+			printf("Attention : la chaîne en paramètre est trop longue, et sera réduite à une longueur max_size valant %lu \n", max_size);
+		}
+		len++;
+	}
+	return len;
+}
+
+size_t String::getCapacity(size_t size){
+	size=size*2;
+	if (size>(max_size/2))
+		return max_size;
+	else
+		return size;
+}
--- string.h
+++ string.h
@@ -3,15 +3,20 @@
 
 class String {
   protected :
+	// Attributs
 	size_t size_; //taille effective de la String
     size_t capacity_; //capacité du tableau
     char* tab_; //"valeur" de la String (contenu : chaine de caractères)
     static const size_t max_size; //taille maximale du tableau/chaîne
     
+    //Méthodes
+    size_t String::length(const char* c);
+    size_t String::getCapacity(size_t size);
+    
   public : 
 	//Constructeurs
     String(); //Constructeur par défaut
-	//String(size_t n, char t); //Constructeur à partir d'une c-string
+	//String(char* str_in); //Constructeur à partir d'une c-string
 
 	//Destructeurs
 	//~String();
