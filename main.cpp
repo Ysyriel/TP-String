d4718e683cdc140061b01dd7c9998d794946e6ce
Pierre Caulet <pierre.caulet78@gmail.com>
2017-01-23 15:05:27 +0100

    Ajout du constructeur par défaut et test.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "string.h"
 
 int main(){
+	String str;  
+	printf("a%sb", str);   //test avec gdb $1 = {size_ = 0, capacity_ = 0, static max_size = 100, tab_ = 0x602010 ""}
+
 	return EXIT_SUCCESS;
 }
 
--- string.cpp
+++ string.cpp
@@ -1,8 +1,14 @@
 #include "string.h"
 #include <cstdio>
 #include <cstdlib>
+
 const size_t String::max_size = 100; //Définition de l'attribut static
 
-
+String::String(){
+	size_=0;
+	capacity_=0;
+	tab_=new char[capacity_+1];
+	tab_[0]='\0';
+}
 
 
